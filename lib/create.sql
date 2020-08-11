880b738bfa5d6888e8d1c6ec6591c098dddd1063
Michael Waide <mvwaide@gmail.com>
2020-08-11 15:38:14 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,21 +1,21 @@
 CREATE TABLE projects (
   id INTEGER PRIMARY KEY,
   title TEXT,
-  catagory TEXT,
-  funding_goal INTEGER,
-  start_date DATE,
-  end_date DATE
+  category TEXT,
+  funding_goal NUMERIC,
+  start_date TEXT,
+  end_date TEXT
 );
 
 CREATE TABLE users (
   id INTEGER PRIMARY KEY,
-  name TEXT
+  name TEXT,
   age INTEGER
 );
 
 CREATE TABLE pledges (
   id INTEGER PRIMARY KEY,
-  amount INTEGER,
-  user_id INTEGER,
-  project_id INTEGER
-);
+  amount NUMERIC,
+  project_id INTEGER,
+  user_id INTEGER
+); 
